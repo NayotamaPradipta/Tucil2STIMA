6c73d224f6981a3b6ff35e169565bed173f5a9d5
Nayotama Pradipta <n4y0t4m4@gmail.com>
2022-02-28 00:29:33 +0700

    Add function to get extreme points

--- src/myConvexhull.py
+++ src/myConvexhull.py
@@ -1,5 +1,5 @@
 # File implementasi Convex hull dengan divide & conquer
-
+import numpy as np 
 def determinantThreePoints(p1, p2, pi):
     # Mengembalikan determinan dari tiga titik koordinat
     return (p1[0] * p2[1] + pi[0] * p1[1] + p2[0] * pi[1] - pi[0] * p2[1] - p2[0] * p1[1] - p1[0] * pi[1])
@@ -10,3 +10,13 @@ def up(p1, p2, pi):
 
 def ignoreCoordinate(p1, p2, pi):
     return ((p1[0] * (p2[1] - pi[1]) + p2[0] * (pi[1] - p1[1]) + pi[0] * (p1[1] - p2[1])) == 0)
+
+def getExtremeLeftPoint(bucket):
+    minXval = min(bucket[:, 0])
+    minXidx = np.where(bucket[:,0]==minXval)
+    return bucket[minXidx]
+
+def getExtremeRightPoint(bucket):
+    maxXval = max(bucket[:, 0])
+    maxXidx = np.where(bucket[:,0] == maxXval)
+    return bucket[maxXidx]
