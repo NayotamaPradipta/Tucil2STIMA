b01a66cbdf118ffa808873e99aee45c7b67572a5
Nayotama Pradipta <n4y0t4m4@gmail.com>
2022-02-28 00:11:11 +0700

    Add supporting functions

--- src/myConvexhull.py
+++ src/myConvexhull.py
@@ -1,1 +1,12 @@
 # File implementasi Convex hull dengan divide & conquer
+
+def determinantThreePoints(p1, p2, pi):
+    # Mengembalikan determinan dari tiga titik koordinat
+    return (p1[0] * p2[1] + pi[0] * p1[1] + p2[0] * pi[1] - pi[0] * p2[1] - p2[0] * p1[1] - p1[0] * pi[1])
+
+def up(p1, p2, pi):
+    return (determinantThreePoints(p1, p2, pi) > 0)
+
+
+def ignoreCoordinate(p1, p2, pi):
+    return ((p1[0] * (p2[1] - pi[1]) + p2[0] * (pi[1] - p1[1]) + pi[0] * (p1[1] - p2[1])) == 0)
